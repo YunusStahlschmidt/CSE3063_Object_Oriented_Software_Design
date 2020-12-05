998c96014a5f502730eac8fec2e69d46763aca60
Sam Kunbargi <48989642+SamNK@users.noreply.github.com>
2020-12-05 13:24:27 +0300

    loggers added

--- OOP_Project/Main.java
+++ OOP_Project/Main.java
@@ -5,14 +5,18 @@ import java.util.Date;
 import java.util.Random;
 import java.util.Scanner;
 
-// import sun.jvm.hotspot.utilities.IntArray;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+// import sun.jvm.hotspot.utilities.IntArray;
 
 /* 
 This class will where al "computations" will take place
 */
 
 public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
 
         // Required Objects
@@ -32,6 +36,7 @@ public class Main {
 
                 // parsing given json file
                 parser.parseJSONFile(jsonPath, dataset);
+                logger.info("data set was parsed successfully");
 
                 // Getting users path from client
                 System.out.print("Please type the absolute path of config.json file: ");
@@ -40,9 +45,13 @@ public class Main {
 
                 // parsing given config.json file
                 parser.parseConfigFile(configPath, users);
+                logger.info("config file was parsed successfully");
+
                 break;
             } catch (Exception e) {
                 System.out.println("FileNotFound error has been occured! Please check your file paths.");
+                logger.warn("FileNotFound error has occured!");
+
             }
         }
 
@@ -51,31 +60,55 @@ public class Main {
         long randomLabel;
         ArrayList<Label> tempLabels = dataset.getLabels();
 
-        for (Instance anInstance: dataset.getInstances()) {
+        for (Instance anInstance : dataset.getInstances()) {
             userIndex = random.nextInt(users.size());
             addedLabels = new ArrayList<>();
 
-            for (int maxlabel = 1; maxlabel <= random.nextInt((int)dataset.getMaxLabel())+1;) {
-                randomLabel = tempLabels.get( random.nextInt(tempLabels.size()) ).getId();
+            for (int maxlabel = 1; maxlabel <= random.nextInt((int) dataset.getMaxLabel()) + 1;) {
+                randomLabel = tempLabels.get(random.nextInt(tempLabels.size())).getId();
 
                 if (!addedLabels.contains(randomLabel)) {
                     addedLabels.add(randomLabel);
                     maxlabel++;
                 }
-                
+
             }
-            labelAssignments.add(new LabelAssignment(anInstance.getId(), addedLabels, users.get(userIndex).getId(), new Date()));
+            labelAssignments.add(
+                    new LabelAssignment(anInstance.getId(), addedLabels, users.get(userIndex).getId(), new Date()));
         }
         System.out.println(labelAssignments);
 
-        for (LabelAssignment lAssignment : labelAssignments) { 
+        for (LabelAssignment lAssignment : labelAssignments) {
             System.out.println();
             System.out.println(String.format("Instance Id: %d", lAssignment.getInstanceId()));
             System.out.println("Assigned Label Id: " + lAssignment.getAssignedLabelId());
             System.out.println("User Id: " + lAssignment.getUserId());
             System.out.println("Date: " + lAssignment.getDate());
-        }  
-        
+
+            long user_id = lAssignment.getUserId();
+            String user_name = users.get((int) lAssignment.getUserId() - 1).getName();
+            long instance_id = lAssignment.getInstanceId();
+            String instance_text = dataset.getInstances().get((int) lAssignment.getInstanceId() - 1).getText();
+            long class_label_id = lAssignment.getInstanceId();
+
+            if (lAssignment.getAssignedLabelId().size() == 1) {
+                String label_name = dataset.getLabels().get((int) lAssignment.getSpecificAssignedLabelId(0) - 1)
+                        .getName();
+                logger.info("user id:" + user_id + " " + user_name + " tagged instance id:" + instance_id
+                        + " with class label " + class_label_id + ":" + label_name + " instance: " + instance_text);
+            } else {
+                ArrayList<String> labels = new ArrayList<>();
+
+                for (int n = 0; n < lAssignment.getAssignedLabelId().size(); n++) {
+                    labels.add(dataset.getLabels().get((int) lAssignment.getSpecificAssignedLabelId(n) - 1).getName());
+                }
+                logger.info("user id:" + user_id + " " + user_name + " tagged instance id:" + instance_id
+                        + " with class labels " + lAssignment.getAssignedLabelId().toString() + ":" + labels.toString()
+                        + " instance: " + instance_text);
+            }
+
+        }
+
         while (true) {
             try {
                 System.out.print("Please type the absolute path of output file: ");
@@ -84,10 +117,13 @@ public class Main {
 
                 // serializing to json file
                 serializer.serializeJSONFile(outputPath, dataset, labelAssignments, users);
+
                 break;
             } catch (Exception e) {
                 System.out.println("File not found! Please make sure you provided a correct path.");
+                logger.warn("Output File not found!");
             }
         }
+        logger.info("Program Excuted Successfully");
     }
 }
