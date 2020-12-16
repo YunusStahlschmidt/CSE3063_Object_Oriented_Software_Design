a3b6b097e0a23ae6337f54619be27887b60663b1
Huzeyfe Ayaz <huzeyfeayaz23@hotmail.com>
2020-12-17 00:27:39 +0300

    Parser.java file has been updated (not-completed)

--- OOP_Project/Parser.java
+++ OOP_Project/Parser.java
@@ -1,13 +1,19 @@
 package OOP_Project;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Dictionary;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Scanner;
+
+import javax.xml.crypto.Data;
 
 import org.json.simple.*;
 import org.json.simple.parser.*;
@@ -22,11 +28,13 @@ This class is responsible for parsing the input and config files
 public class Parser {
     private JSONParser jsonParser = new JSONParser();
     private static final Logger logger = LoggerFactory.getLogger(Parser.class);
+    long currentDatasetId;
 
     public Parser() {
     }
 
-    public void parseJSONFile(String jsonPath, Dataset dataset) throws Exception {
+    public Dataset parseJSONFile(String jsonPath) throws Exception {
+        Dataset dataset = new Dataset();
         JSONObject jsonObject = jsonObjectCreator(jsonPath);
 
         // Parsing
@@ -72,20 +80,30 @@ public class Parser {
             }
             dataset.addInstance(id, text);
         }
+        return dataset;
     }
 
-    public void parseConfigFile(String configPath, ArrayList<User> users) throws Exception {
+    public void parseConfigFile(String configPath, ArrayList<User> users, HashMap<Long, Dataset> datasets) throws Exception {
         JSONObject jsonObject = jsonObjectCreator(configPath);
 
         // Parsing
+        currentDatasetId = (long) jsonObject.get("CurrentDatasetId");
         JSONArray newUserArray = (JSONArray) jsonObject.get("users");
+        JSONArray newDatasetArray = (JSONArray) jsonObject.get("datasets");
+        URL url;
+        File file;
+
         // System.out.println(newUserArray);
+
+        // Users
         Iterator<Map.Entry> itr1;
         Iterator itr2 = newUserArray.iterator();
+        User newUser;
         long id = 0;
         String name = "";
-        String text = "";
-
+        String userType = "";
+        JSONArray assignedDatasets = new JSONArray();
+        
         while (itr2.hasNext()) {
             itr1 = ((Map) itr2.next()).entrySet().iterator();
             while (itr1.hasNext()) {
@@ -94,12 +112,38 @@ public class Parser {
                     id = (long) pair.getValue();
                 } else if (pair.getKey().equals("user name")) {
                     name = (String) pair.getValue();
+                } else if (pair.getKey().equals("user type")) {
+                    userType = (String) pair.getValue();
                 } else {
-                    text = (String) pair.getValue();
+                    // System.out.println(jsonObject.get("assigned datasets").getClass().getName());
+                    assignedDatasets = (JSONArray) jsonObject.get("assigned datasets");
                 }
             }
-            users.add(new User(id, name, text));
-            logger.info("userManager: created " + name + " as " + text);
+            newUser = new User(id, name, userType);
+            newUser.setNumberOfDatasetsAssigned(assignedDatasets.size());
+            System.out.println("NumberOfDatasetsAssigned" + assignedDatasets.size());
+            users.add(newUser);
+            logger.info("userManager: created " + name + " as " + userType);
+        }
+
+        // Datasets
+        Long datasetId = (long) 0;
+        String datasetPath = "";
+
+        itr2 = newDatasetArray.iterator();
+        while (itr2.hasNext()) {
+            itr1 = ((Map) itr2.next()).entrySet().iterator();
+            while (itr1.hasNext()) {
+                Map.Entry pair = itr1.next();
+                if (pair.getKey().equals("dataset id")) {
+                    datasetId = (long) pair.getValue();
+                } else if (pair.getKey().equals("path")) {
+                    datasetPath = (String) pair.getValue();
+                }
+            }
+            // url = getClass().getResource(datasetPath);
+            // file = new File(url.getPath());
+            datasets.put(datasetId, parseJSONFile(datasetPath));
         }
     }
 
@@ -107,5 +151,9 @@ public class Parser {
         FileReader reader = new FileReader(path);
         Object obj = jsonParser.parse(reader);
         return (JSONObject) obj;
+    }
+
+    public long getCurrentDatasetId() {
+        return currentDatasetId;
     }
 }
