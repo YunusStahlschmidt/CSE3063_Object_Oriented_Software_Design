00baed319bf530ac7d3dcc7269f14ce4682fb936
SamNK <smartsameeh@gmail.com>
2021-01-13 13:23:10 +0300

    attendence class created

--- Python-Project/Iteration-1/Artifacts/answer.py
+++
@@ -1,6 +0,0 @@
-class Answer(object):
-    """
-    docstring
-    """
-    def __init__(self, text):
-        self.answer_text = text
--- Python-Project/Main/main.py
+++ Python-Project/Main/main.py
@@ -1,11 +1,13 @@
 from parser_class import *
 from poll_calculation import *
+from attendence import *
 import pandas as pd
 import numpy as np
 import pprint
 import os
 if __name__ == "__main__":
     parser_obj = Parser()
+    attendence_obj = attendence()
     parser_obj.parse_students()
     parser_obj.parse_answer_keys()
     parser_obj.parse_poll_reports()
@@ -21,5 +23,7 @@ if __name__ == "__main__":
             # print(calculations.student_array_for7a)
             df1 = pd.DataFrame(calculations.student_array_for7a)
             df1.to_excel(os.path.join(calculations.POLL_PATH, f'{poll.poll_title}.xlsx'))
+            
+    attendence_obj.create_attendance_file(parser_obj)
 
         
