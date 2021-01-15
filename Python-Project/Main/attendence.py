5ba6a517084b908a8c5c383fb74c11d876e1a589
muhammed-oztel <ffatihoztel@gmail.com>
2021-01-15 23:26:14 +0300

    Global file is created, have to fix some issues

--- Python-Project/Main/attendence.py
+++ Python-Project/Main/attendence.py
@@ -17,6 +17,9 @@ class attendence(object):
                 print(poll.poll_title, len(poll.attended_students), len(set(poll.attended_students)))
                 for student in poll.attended_students:
                     student.student_attendance += 1
+
+                    
+        # can be moved to output
         filename = os.path.join(self.STAT_PATH, "Attendance.xlsx")
         with xlsxwriter.Workbook(filename) as workbook:
             worksheet = workbook.add_worksheet("attendance")
@@ -27,9 +30,10 @@ class attendence(object):
             worksheet.write("B1", "Student Id", bold)
             worksheet.write("C1", "Name", bold)
             worksheet.write("D1", "Surname", bold)
-            worksheet.write("E1", "Number of Attendance Polls", bold)
-            worksheet.write("F1", "Attendance Rate", bold)
-            worksheet.write("G1", "Attendance Percentage", bold)
+            worksheet.write("E1", "Remark", bold)
+            worksheet.write("F1", "Number of Attendance Polls", bold)
+            worksheet.write("G1", "Attendance Rate", bold)
+            worksheet.write("H1", "Attendance Percentage", bold)
 
             for index, student in enumerate(parser.student_list, start=2):
 
@@ -37,10 +41,11 @@ class attendence(object):
                 worksheet.write(f"B{index}", str(student.student_id))
                 worksheet.write(f"C{index}", str(student.student_name))
                 worksheet.write(f"D{index}", str(student.student_surname))
-                worksheet.write(f"E{index}", str(self.numOfAttendance))
-                worksheet.write(f"F{index}", str(f"attended {student.student_attendance} of {self.numOfAttendance} courses"))
+                worksheet.write(f"E{index}", str(student.remark))
+                worksheet.write(f"F{index}", str(self.numOfAttendance))
+                worksheet.write(f"G{index}", str(f"attended {student.student_attendance} of {self.numOfAttendance} courses"))
                 if self.numOfAttendance == 0:
-                    worksheet.write(f"G{index}", str(f"{0}%"))
+                    worksheet.write(f"H{index}", str(f"{0}%"))
                 else:
-                    worksheet.write(f"G{index}", str(f"{round((student.student_attendance/self.numOfAttendance)*100)}%"))
+                    worksheet.write(f"H{index}", str(f"{round((student.student_attendance/self.numOfAttendance)*100)}%"))
         logging.info('Attendence file created sucessfully')
--- Python-Project/Main/output.py
+++ Python-Project/Main/output.py
@@ -1,5 +1,22 @@
+import json
+import pandas as pd
 class Output(object):
     """
     docstring
     """
-    pass
+    def __init__(self, student_list, polls):
+        self.__student_list = student_list
+        self.__polls = polls
+        self.__output_dict = [] # list of dict -> key : student id , value list -> name, surname , mail, dict--> quiz infos, quiz name quiz num of question quiz ...
+                                                                                                            # sort with lambda function    
+                                                                                            
+    
+    def add_to_global_list(self):
+        # for student_obj in __student_list:
+        #     for poll in __polls:
+
+        pass
+    
+    def output_global_file(self):
+        
+        pass
